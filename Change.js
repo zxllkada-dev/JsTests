1b3c7733d077283c810384998f2b30a58b034c3e
Kada <103851762+zxllkada-dev@users.noreply.github.com>
2024-01-11 03:33:54 +0100

    Update Change.js

--- Change.js
+++ Change.js
@@ -1,3 +1,3 @@
-document.querySelectorAll('.nobtmpad, .res_heading_right').forEach(function(element) { 
+document.querySelectorAll('.nobtmpad, .res_heading_right, .textbox_small').forEach(function(element) { 
   element.style.display = 'none'; 
 });
