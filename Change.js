2411c3783ba8b3c82aa075ec05a3e18c9e0ed36c
Kada <103851762+zxllkada-dev@users.noreply.github.com>
2024-01-11 04:40:26 +0100

    Update Change.js

--- Change.js
+++ Change.js
@@ -1,8 +1,3 @@
 document.querySelectorAll('.nobtmpad, .res_heading_right, .textbox_small, .plain_link').forEach(function(element) { 
   element.style.display = 'none'; 
 });
-
-document.querySelectorAll('.HackButton').forEach(function(element) { 
-  element.style.padding = '10px 20px';
-  element.style.background = '#1a73e8';
-});
